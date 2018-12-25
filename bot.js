85cbf9f84453995a24aa2535ce03fbadc62a0e7a
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-25 10:34:58 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,13 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-
-
-// playing
 client.on('ready', () => {                           
-client.user.setGame(`Nova Codes ♥♥`,'https://www.twitch.tv/tarikrs');                                                                                                                                                                                                                                                                                                                                                                                                                             client.user.setGame(`-help | On 20 Servers`,'https://www.twitch.tv/v5bz');
-});          
-
+client.user.setGame(`Nova Codes ♥♥`,'https://www.twitch.tv/Nova Codes ♥♥');
+});
 client.on('message', message => {
   let anything = [
     "وعليكم السلام",
@@ -18,6 +14,31 @@ client.on('message', message => {
     
   ]
   if(message.content === '-codenew'){
+    message.reply(`${anything[Math.floor(Math.random() * anything.length)]}`)
+    
+    return;
+  }
+});
+client.on('message', message => {
+  let anything = [
+    "```client.on('message', message => {
+              if (!message.channel.guild) return;
+      if(message.content =='#count')
+      var n3k4a = new Discord.RichEmbed()
+      .setThumbnail(message.author.avatarURL)
+      .setFooter(message.author.username, message.author.avatarURL)
+      .setTitle('🌷| Members info')
+      .addBlankField(true)
+      .addField('عدد اعضاء السيرفر',`${message.guild.memberCount}`)
+      message.channel.send(geky );
+    });```",
+    "ياهلا والله",
+    "وعليكم السلام ورحمة الله وبركاته",
+    "مرحبا بك في السيرفر"
+    
+    
+  ]
+  if(message.content === '-codeonline'){
     message.reply(`${anything[Math.floor(Math.random() * anything.length)]}`)
     
     return;
