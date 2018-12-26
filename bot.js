974f5c6f7158e4a42c99306d3ea18914e2413b0d
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-26 10:33:15 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -552,18 +552,7 @@ client.on('message', message => {
    });
 client.on('message', msg => {
   if (msg.content === '-codepls') {
-    msg.reply('client.on('```message',async msg => {//Toxic Codes
-  var p = "-";//Toxic Codes
-  if(msg.content.startsWith(p + "setuser")) {//Toxic Codes
-  if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **ليس لديك صلاحيه**');//Toxic Codes
-  if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');//Toxic Codes
-  msg.guild.createChannel(`Members : ◤ → ${client.users.size} ← ◢` , 'voice').then(time => {//Toxic Codes
-    });//Toxic Codes
-
-  }
- 
-});```')
+    msg.reply('client.on('``كسمككك```')
   }
 })
-});
 client.login(process.env.BOT_TOKEN);
