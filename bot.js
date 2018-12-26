c281aa2af3d79b94b98e83342b401ebb76059d2b
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-26 15:16:55 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -588,19 +588,4 @@ client.on('message', message => {
      }
        });
 
-client.on('message', msg => {
-
-    if (msg.content == '-join') {
-        if (msg.member.voiceChannel) {
-
-     if (msg.member.voiceChannel.joinable) {
-         msg.member.voiceChannel.join().then(msg.react('white_check_mark'));
-     }
-    }
-}
-})
-client.on('ready', () => { //code bot not leave room voice //Bot Is Online
-    client.channels.get("527114990751055893").join(); //by : Toxic Codes
-    });
-
 client.login(process.env.BOT_TOKEN);
