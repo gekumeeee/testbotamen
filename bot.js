8c063134ea611064e05422c7dec0bb457676196d
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-26 09:56:11 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -556,8 +556,7 @@ client.on('message', message => {
   if (message.content.startsWith(prefix + "codevoice")) {
   let embed = new Discord.RichEmbed()
       .setColor("RANDOM")
-      .setDescription(```
-client.on('message',async msg => {//Nova codes
+      .setDescription(` client.on('message',async msg => {//Nova codes
   var p = "-";//Toxic Codes
   if(msg.content.startsWith(p + "setuser")) {//Nova codes
   if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **ليس لديك صلاحيه**');//Toxic Codes
@@ -567,8 +566,7 @@ client.on('message',async msg => {//Nova codes
 
   }
  
-});
- ```)
+}); `)
    message.channel.sendEmbed(embed)
    
    }
