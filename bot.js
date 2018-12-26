b21942cbd70b5e128aa95b261c68e78ff4dabba9
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-26 16:04:51 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,8 +5,6 @@ const client = new Discord.Client();
 
 // playing
 client.on('ready', () => {                           
-client.user.setGame(`♥ Premium ℕova ♥ `,'https://www.twitch.tv/ℕ o v a Codes♥');
-  });
 
 
 client.on('message', message => {
@@ -587,5 +585,28 @@ client.on('message', message => {
 
      }
        });
-
+const adminprefix = "-";
+const devs = ['351366504068939777'];
+client.on('message', message => {
+  var argresult = message.content.split(`).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+    
+if (message.content.startsWith(adminprefix + 'setG')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(${argresult} تم تغيير بلاينق البوت إلى )
+} else 
+  if (message.content.startsWith(adminprefix + 'setN')) {
+client.user.setUsername(argresult).then
+    message.channel.sendMessage(${argresult} : تم تغيير أسم البوت إلى)
+return message.reply("**لا يمكنك تغيير الاسم يجب عليك الانتظآر لمدة ساعتين . **");
+} else
+  if (message.content.startsWith(adminprefix + 'setA')) {
+client.user.setAvatar(argresult);
+  message.channel.sendMessage(${argresult} : تم تغير صورة البوت);
+      } else     
+if (message.content.startsWith(adminprefix + 'setT')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/N O V A A♥");
+    message.channel.sendMessage(تم تغيير تويتش البوت إلى  ${argresult}`)
+}
+});
 client.login(process.env.BOT_TOKEN);
