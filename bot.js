3f835425327ffbbba24309909ae34446c00a95fc
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-26 09:53:02 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -553,14 +553,13 @@ client.on('message', message => {
 
 client.on('message', message => {
               var prefix = "-" ;
-  if (message.content.startsWith(prefix + "codeonline")) {
+  if (message.content.startsWith(prefix + "codevoice")) {
   let embed = new Discord.RichEmbed()
       .setColor("RANDOM")
       .setDescription(`
-
-client.on('message',async msg => {//Toxic Codes
+client.on('message',async msg => {//Nova codes
   var p = "-";//Toxic Codes
-  if(msg.content.startsWith(p + "setuser")) {//Toxic Codes
+  if(msg.content.startsWith(p + "setuser")) {//Nova codes
   if(!msg.guild.member(msg.author).hasPermissions('MANAGE_CHANNELS')) return msg.reply('❌ **ليس لديك صلاحيه**');//Toxic Codes
   if(!msg.guild.member(client.user).hasPermissions(['MANAGE_CHANNELS'])) return msg.reply('❌ **البوت لا يمتلك صلاحية**');//Toxic Codes
   msg.guild.createChannel(`Members : ◤ → ${client.users.size} ← ◢` , 'voice').then(time => {//Toxic Codes
