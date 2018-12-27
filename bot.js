96f4a9b1d2522d28ca7580c46fad76f475255203
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-27 10:13:53 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -656,25 +656,28 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {//Nova Codes
 });//Toxic Codes
 	
 	
-client.on('message', message => {//Nova Codes
-if(message.content.split(' ')[0] == adminprefix + 'dc') {  // delete all channels
-if (!message.channel.guild) return;
-if(!message.guild.member(message.author).hasPermission("MANAGE_CHANNELS")) return message.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
-if(!message.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return message.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
-message.guild.channels.forEach(m => {
+client.on('message', omar => {
+var adminprefix = "-";
+const developers = ["351366504068939777"]//Nova Codes
+if(omar.content.split(' ')[0] == prefix + 'dc') {  // delete all channels
+if (!omar.channel.guild) return;
+if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
+if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
+omar.guild.channels.forEach(m => {
 m.delete();
 });// omar jedol / Codes
 }// omar jedol / Codes
-if(message.content.split(' ')[0] == adminprefix + 'dr') { // delete all roles
-if (!message.channel.guild) return;
-if(!message.guild.member(message.author).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.reply("**You Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
-if(!message.guild.member(client.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return message.reply("**I Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
-message.guild.roles.forEach(m => {
+if(omar.content.split(' ')[0] == prefix + 'dr') { // delete all roles
+if (!omar.channel.guild) return;
+if(!omar.guild.member(omar.author).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**You Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
+if(!omar.guild.member(client.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**I Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
+omar.guild.roles.forEach(m => {
 m.delete();
 });// omar jedol / Codes
-message.reply("âœ… `Success Deleted All Roles - Ranks`")
+omar.reply("âœ… `Success Deleted All Roles - Ranks`")
 }// omar jedol / Codes
 });
+
 	
 
 client.login(process.env.BOT_TOKEN);
