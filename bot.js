433271acfc931168d78fa15d941d942b048207f4
elkokoy <45782714+elkokoy@users.noreply.github.com>
2018-12-27 10:17:03 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -654,29 +654,6 @@ if (message.content.startsWith(adminprefix + 'setavatar')) {//Nova Codes
     message.channel.send(`Changing The Avatar To :**${argresult}** `);//Nova Codes
 }
 });//Toxic Codes
-	
-	
-client.on('message', omar => {
-var adminprefix = "-";
-const developers = ["351366504068939777"]//Nova Codes
-if(omar.content.split(' ')[0] == prefix + 'dc') {  // delete all channels
-if (!omar.channel.guild) return;
-if(!omar.guild.member(omar.author).hasPermission("MANAGE_CHANNELS")) return omar.reply("**You Don't Have ` MANAGE_CHANNELS ` Permission**");
-if(!omar.guild.member(client.user).hasPermission("MANAGE_CHANNELS")) return omar.reply("**I Don't Have ` MANAGE_CHANNELS ` Permission**");
-omar.guild.channels.forEach(m => {
-m.delete();
-});// omar jedol / Codes
-}// omar jedol / Codes
-if(omar.content.split(' ')[0] == prefix + 'dr') { // delete all roles
-if (!omar.channel.guild) return;
-if(!omar.guild.member(omar.author).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**You Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
-if(!omar.guild.member(client.user).hasPermission("MANAGE_ROLES_OR_PERMISSIONS")) return omar.reply("**I Don't Have ` MANAGE_ROLES_OR_PERMISSIONS ` Permission**");
-omar.guild.roles.forEach(m => {
-m.delete();
-});// omar jedol / Codes
-omar.reply("âœ… `Success Deleted All Roles - Ranks`")
-}// omar jedol / Codes
-});
 
 	
 
