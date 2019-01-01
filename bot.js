3f280a251e93c19ea8d80c447b832b797d922814
elkokoy <45782714+elkokoy@users.noreply.github.com>
2019-01-01 19:54:42 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -987,18 +987,23 @@ client.on("message", (message) => {
     }
  
  
-  if (message.content.startsWith("-close")) {
+ if (message.content.startsWith("-close")) {
         if (!message.channel.name.startsWith(`ticket-`)) return message.channel.send(`You can't use the close command outside of a ticket channel.`);
  
-       message.channel.send(`سيتم اقفال التذكره الان`)
+       message.channel.send(`هل انت متأكد من اقفالك للتذكرة اذا متأكد اكتب -confirm`)
            .then((m) => {
+               message.channel.awaitMessages(response => response.content === '-confirm', {
+                       max: 1,
+                       time: 10000000,
+                       errors: ['time'],
+                   })    /// DREAM
+                   .then((collected) => {
                        message.channel.delete();
                    })    /// DREAM
                    .catch(() => {
                        m.edit('Ticket close timed out, the ticket was not closed.').then(m2 => {
                            m2.delete();
                        }, 3000000);
- 
                    });
            });
    }
