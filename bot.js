530965a0afe708cddbee58ae0ba2e872a9522c14
elkokoy <45782714+elkokoy@users.noreply.github.com>
2019-01-08 01:16:59 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1095,29 +1095,6 @@ client.on('message', function(msg) {
     }
 });
 
-client.on('message', message => {
-            if (message.content.startsWith("!helpcb")) {
-     let embed = new Discord.RichEmbed()
-.setAuthor(message.guild.name, message.guild.iconURL)
-        .setDescription(``
-**
-const Discord = require('discord.js');
-const client = new Discord.Client();
 
-client.on('ready', () => {
-  console.log(Logged in as ${client.user.tag}!);
-});
-
-client.on('message', msg => {
-  if (msg.content === 'Hi') {
-    msg.reply('Hello');
-  }
-});
-
-client.login('token');                
-**``)
-      message.author.sendEmbed(embed);
-    }
-});
 
 client.login(process.env.BOT_TOKEN);
